d29418d443b9649e3cc9e0d83b45b9dc4e2fe71c
L Vinicius J <lviniciusj@outlook.com>
2017-09-09 16:40:59 -0300

    Tuto7-ordenacaoOK

--- BibliotecaMVC/BibliotecaMVC/Models/Livro.cs
+++ BibliotecaMVC/BibliotecaMVC/Models/Livro.cs
@@ -22,15 +22,5 @@ namespace BibliotecaMVC.Models
 
         public ICollection<LivroEmprestimo> LivroEmprestimos { get; set; }
         public ICollection<LivroAutor> LivroAutores { get; set; }
-
-        //Ctrl + K + C (Comment)
-        //Ctrl + K + U (Uncomment)
-        //[Required(ErrorMessage = "Campo Obrigatório!")]
-        //[DataType(DataType.)]//CreditCard,Currency,Custom,Date,DateTime,Duration,EmailAddress,Html,ImageUrl,MultilineText,Password,PhoneNumber,PostalCode,Text,Time,Upload,Url
-        //[Display(Name = "", Description = "")]
-        //[DisplayFormat(DataFormatString = "dd/mm/yyyy")]
-        //[MaxLength(150, ErrorMessage = "O Campo nome pode ter no máximo {0} carácteres!"), MinLength(3, ErrorMessage = "O campo nome deve possuir no mínimo {0} caracteres!")]
-        //[RegularExpression(".+\\@.+\\..+",ErrorMessage ="Informe um e-mail válido!")]
-        //public string X { get; set; }
     }
 }
